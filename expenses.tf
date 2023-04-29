7f4a97f9830bd7a6232a6d912b5a982d95924d92
Dilshod0777 <dilshod0777@users.noreply.github.com>
2023-04-28 19:12:45 -0500

    2change

--- expenses.tf
+++ expenses.tf
@@ -1,10 +1,10 @@
-THIS FILE TO MANAGE ALL EXPENSES
+iTHIS FILE TO MANAGE ALL EXPENSES
 
  rent     2000
  food cost  1000
  phone bill  1440
- gas
- household items
+ gas         500
+ household items  200
  electric bill
  fuel
  game
